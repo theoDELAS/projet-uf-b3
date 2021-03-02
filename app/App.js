65350bd1aa925fc282f219b8057b152f255bdbc0
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-02 16:12:54 +0100

    :sparkles: call api routes to test, its working

--- app/App.js
+++ app/App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import axios from 'axios'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import UserService from './services/UserService';
