ca7766f3514608c75c642e651268044ece32a144
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-02 16:11:32 +0100

    :zap: setting up axios request

--- app/App.js
+++ app/App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
+import axios from 'axios'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import UserService from './services/UserService';
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+
+  state = {
+    userList: []
+  }
+
+  componentDidMount() {
+    UserService.getAll()
+      .then(res=> {
+        const userList = res.data['hydra:member']
+        this.setState({userList})
+      })
+  }
+
+  render() {
+    const {userList} = this.state;
+    console.log(userList.map(user=> user.username))
+
+    return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <h1>Users BDD</h1>
+        {
+          userList.map(user=> <Text key={user.id}>{user.username}</Text>)
+        }
     </View>
-  );
+    )
+  };
 }
 
 const styles = StyleSheet.create({
