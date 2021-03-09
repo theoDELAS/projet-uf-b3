c7b6a6a0a47179e1bea91541041dd1f37eec8eec
Théo <theo.delas@ynov.com>
2021-03-09 11:18:47 +0100

    :twisted_rightwards_arrows: rebase tabbar with dev-app

---
+++ app/src/screens/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView  } from 'react-native';
+
+const ProfileScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>Mon profil</Text>
+    </SafeAreaView>
+  );
+}
+
+export default ProfileScreen;
---
+++ app/src/screens/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView  } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>Rechercher</Text>
+    </SafeAreaView>
+  );
+}
+
+export default SearchScreen;
