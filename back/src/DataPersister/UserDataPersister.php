0e1c48a084f4695351f61a54897fca193411f2c5
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-09 11:13:48 +0100

    :sparkles: a persister to hash password

--- back/src/DataPersister/UserDataPersister.php
+++ back/src/DataPersister/UserDataPersister.php
@@ -3,8 +3,61 @@
 
 namespace App\DataPersister;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserDataPersister
+
+class UserDataPersister implements ContextAwareDataPersisterInterface
 {
+    private $_entityManager;
+    private $_passwordEncoder;
 
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UserPasswordEncoderInterface $passwordEncoder
+    ) {
+        $this->_entityManager = $entityManager;
+        $this->_passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    public function supports($data, array $context = []): bool
+    {
+        // TODO: Implement supports() method.
+        return $data instanceof User;
+    }
+
+    /**
+     * @param User $data
+     */
+    public function persist($data, array $context = [])
+    {
+        // TODO: Implement persist() method.
+        if ($data->getPlainPassword()) {
+            $data->setPassword(
+                $this->_passwordEncoder->encodePassword(
+                    $data,
+                    $data->getPlainPassword()
+                )
+            );
+            $data->eraseCredentials();
+        }
+
+        $this->_entityManager->persist($data);
+        $this->_entityManager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($data, array $context = [])
+    {
+        // TODO: Implement remove() method.
+        $this->_entityManager->remove($data);
+        $this->_entityManager->flush();
+    }
 }
