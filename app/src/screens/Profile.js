6ad4765ef91617868f327fb99564a80e5c6ff6ed
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-16 09:56:30 +0100

    :zap: try to get inventory

--- app/src/screens/Profile.js
+++ app/src/screens/Profile.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, SafeAreaView  } from 'react-native';
 
+import UserService from '../../services/UserService'
+
 const ProfileScreen = () => {
+  const [user, setUser] = useState({})
+  const [inventory, setInventory] = useState({})
+
+  useEffect(() => {
+    getUser(),
+    getInventory()
+  }, [])
+
+  const getUser = () => {
+    UserService.getOne()
+      .then(res =>  {
+        const user = res.data
+        console.log(user)
+        setUser(user)
+      }, [])
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  const getInventory = () => {
+    UserService.getInventory()
+      .then(res => {
+        const inventory = res.data
+        console.log(inventory)
+        setInventory(inventory)
+      }, [])
+  }
+
+  console.log(user.steamId)
+
   return (
     <SafeAreaView>
-        <Text>Mon profil</Text>
+        <Text>Mon profil - { user.email }</Text>
     </SafeAreaView>
   );
 }
