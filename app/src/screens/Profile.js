a1be56f0d13023e542b359b65b77251ea50047d7
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-16 18:03:08 +0100

    :sparkles: parse api steam object to get inventory items

--- app/src/screens/Profile.js
+++ app/src/screens/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Text, SafeAreaView  } from 'react-native';
+import { Text, SafeAreaView, Image, ScrollView  } from 'react-native';
+import { Card, List, Title } from 'react-native-paper'
 
 import UserService from '../../services/UserService'
 
@@ -28,16 +29,77 @@ const ProfileScreen = () => {
     UserService.getInventory()
       .then(res => {
         const inventory = res.data
-        console.log(inventory)
-        setInventory(inventory)
+        // console.log(inventory.rgDescriptions)
+        // const result = []
+        for(const test in inventory.rgDescriptions) {
+          const tried = inventory.rgDescriptions[test]
+          // console.log(tried)
+          // result.push(tried)
+          setInventory(tried)
+
+          // for(const again in tried) {
+          //   console.log(tried[again])
+          // }
+          // console.log(inventory.rgDescriptions[test])
+        }
+        // setInventory(tried)
       }, [])
   }
 
-  console.log(user.steamId)
+  // console.log(user.steamId)
 
   return (
     <SafeAreaView>
+        <ScrollView>
         <Text>Mon profil - { user.email }</Text>
+        {/* <List.Section> */}
+        {/* <List.Subheader>Votre inventaire</List.Subheader> */}
+        {
+          [inventory].map((t) => 
+            // console.log(t.tags)
+
+            [t].map((c) => 
+            <Card key={t.appid}>
+            <Card.Title title="Test" />
+            <Card.Content>
+              <Title>{t.market_hash_name}</Title>
+{
+            // console.log(c.tags)
+            c.tags.map((value, key) => {
+              console.log(t.icon_url)
+
+              console.log(`${value} valeur > ${key}`)
+
+              if(key == 4) {
+                return(<Card.Cover source={{ uri: `https://steamcommunity-a.akamaihd.net/economy/image/${t.icon_url}`}} />)
+
+                // console.log(value)
+                // [value].map((v, k) => {
+                //   if (v == "color") {
+                //     console.log(value[v])
+
+                //   }
+
+                // })
+            // //     // console.log(Object.keys(c.tags)[key])
+            // //     // value.map((v) => 
+            // //     // console.log(v)
+            // //     // )
+              }
+              })
+}
+            </Card.Content>
+            </Card>
+
+            )
+            
+            // <List.Item key={t.appid} title={t.market_hash_name} />
+            // <Text key={i}>{t.market_hash_name}</Text>
+
+          )
+        }
+      {/* </List.Section> */}
+      </ScrollView>
     </SafeAreaView>
   );
 }
