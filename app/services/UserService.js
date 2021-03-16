180bb68471e45a4fe3ffaf9668f8f62057a64e87
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-16 09:55:57 +0100

    :zap: try to get inventory csgo route

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -3,9 +3,33 @@ import axios from "axios";
 const local = 'localhost'
 const device = '192.168.1.36' 
 
+const cors = require('cors')
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+    headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "http://http://localhost:19006/",
+        "Access-Control-Allow-Methods": "*"
+    },// some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 class UserService {
+    login(data) {
+        return axios.post(`http://${device}:8000/api/login`, data)
+    }
+
     getAll() {
-        return axios.get(`http://${local}:8000/api/users`)
+        return axios.get(`http://${device}:8000/api/users`)
+    }
+
+    getOne() {
+        return axios.get(`http://${device}:8000/api/users/20`)
+    }
+
+    getInventory() {
+        return axios.get(`https://steamcommunity.com/id/sheguey667/inventory/json/730/2`, cors(corsOptions))
     }
 }
 
