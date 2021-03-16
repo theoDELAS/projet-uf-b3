d686dc20261c0cf28af4b0c2bcc5ff02c7f0b616
Théo <theo.delas@ynov.com>
2021-03-16 21:22:43 +0100

    :sparkles: Populate left side menu with navigation

--- app/App.js
+++ app/App.js
@@ -3,15 +3,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import MainTabScreen from './src/screens/MainTabScreen';
+import SupportScreen from './src/screens/SupportScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import BookmarkScreen from './src/screens/BookmarkScreen';
+
+
+import { DrawerContent } from './src/screens/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={MainTabScreen} />
-        {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
+      <Drawer.Navigator drawerContent={props => <DrawerContent { ...props } />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
