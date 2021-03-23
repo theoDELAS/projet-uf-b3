2ba764f00f7c4bf645a400d941b7e1f89e874f90
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-23 15:31:29 +0100

    :sparkles: first screens display

--- app/src/screens/Messages.js
+++ app/src/screens/Messages.js
@@ -1,10 +1,55 @@
 import React from 'react';
-import { Text, SafeAreaView  } from 'react-native';
+import { Text, SafeAreaView, ScrollView, Image  } from 'react-native';
+import { Avatar, Divider, List, Title } from 'react-native-paper';
 
 const MessagesScreen = () => {
+
+  const titleCSS = {
+    placement: {
+      textAlign: "center",
+    }
+  }
+
   return (
     <SafeAreaView>
-        <Text>Mes messages</Text>
+      <ScrollView>
+        <Title style={titleCSS.placement}>Messagerie</Title>
+        <List.Item
+          title="xX_TheoGamerzzz_Xx"
+          description="1 Nouveau(x) message(s)"
+          left={props => <List.Icon {...props} icon={{uri:"https://img.icons8.com/cotton/2x/user-male.png"}}/>}
+          >
+        </List.Item>
+        <Divider />
+        <List.Item
+          title="xX_TheoGamerzzz_Xx"
+          description="1 Nouveau(x) message(s)"
+          left={props => <List.Icon {...props} icon={{uri:"https://img.icons8.com/cotton/2x/user-male.png"}}/>}
+          >
+        </List.Item>
+        <Divider />
+        <List.Item
+          title="xX_TheoGamerzzz_Xx"
+          description="1 Nouveau(x) message(s)"
+          left={props => <List.Icon {...props} icon={{uri:"https://img.icons8.com/cotton/2x/user-male.png"}}/>}
+          >
+        </List.Item>
+        <Divider />
+        <List.Item
+          title="xX_TheoGamerzzz_Xx"
+          description="1 Nouveau(x) message(s)"
+          left={props => <List.Icon {...props} icon={{uri:"https://img.icons8.com/cotton/2x/user-male.png"}}/>}
+          >
+        </List.Item>
+        <Divider />
+        <List.Item
+          title="xX_TheoGamerzzz_Xx"
+          description="1 Nouveau(x) message(s)"
+          left={props => <List.Icon {...props} icon={{uri:"https://img.icons8.com/cotton/2x/user-male.png"}}/>}
+          >
+        </List.Item>
+        <Divider />
+      </ScrollView>
     </SafeAreaView>
   );
 }
