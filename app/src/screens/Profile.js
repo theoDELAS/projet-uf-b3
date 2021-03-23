76da01ec312d980434ec827cf4adc6badde14242
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-23 15:30:02 +0100

    :sparkles: some tries to parse api steam inventory objects

--- app/src/screens/Profile.js
+++ app/src/screens/Profile.js
@@ -30,12 +30,16 @@ const ProfileScreen = () => {
       .then(res => {
         const inventory = res.data
         // console.log(inventory.rgDescriptions)
-        // const result = []
+        const result = []
         for(const test in inventory.rgDescriptions) {
           const tried = inventory.rgDescriptions[test]
           // console.log(tried)
-          // result.push(tried)
-          setInventory(tried)
+          result.push(tried)
+          if (!result) {
+            console.log("ERROR")
+          } else {
+            setInventory(result)
+          }
 
           // for(const again in tried) {
           //   console.log(tried[again])
@@ -51,44 +55,83 @@ const ProfileScreen = () => {
   return (
     <SafeAreaView>
         <ScrollView>
-        <Text>Mon profil - { user.email }</Text>
+        <Text>Mon profil - { user.steamId }</Text>
+        <ScrollView>
         {
-          [inventory].map((t) => 
-            [t].map((c) => 
-              <Card key={t.appid}>
-              <Card.Title title="Test" />
-              <Card.Content>
-                <Title>{t.market_hash_name}</Title>
-                {
-                  // console.log(c.tags)
-                  c.tags.map((value, key) => {
-                    console.log(t.icon_url)
+          // console.log(inventory)
+          // [inventory].forEach((i) => {
+          //   // console.log(i)
+          //   [i].map((e) => {
+          //     console.log("array?:"+e)
+          //     // return(
+          //     e.map((t) => {
+          //       // console.log("a")
+          //       // console.log(t.classid)
+          //       // return (
+          //         <Text key={t.classid}>aS</Text>
+          //       // )
+          //     })
+          //     // )
+          //     // [e].forEach((t) =>{
+          //     //   console.log(t)
+          //     // })
+          //   })
+            // i.shift()
+            // return(
+              // let test= JSON.parse(i)
+              // [i].forEach(item => {
+              //   // console.log(item.classid)
+              //   return (
+              //     <Text key={item.classid}>{item.classid}</Text>
+              //   )
+              // })
+              // [i].map((c) => {
+              //   console.log(c.classid)
+              //   return (
+              //     // c.classid.shift()
+              //     // console.log(c.classid + "A")
+              //     console.log("A")
+              //     // new Set(c.classid).size !== c.classid.length
+              //   // console.log(i)
+              //   // <Text key={c.classid}>{c.market_hash_name}</Text>
+              //   )
+              // })
+            // )
+            // return(
+            // [t].slice(0, 2).map((c) => 
+            //   <Card key={c.appid}>
+            //   <Card.Title title="Test" />
+            //   <Card.Content>
+            //     <Title>{t.market_hash_name}</Title>
+            //     {
+            //       // console.log(c.tags)
+            //       c.tags.map((value, key) => {
+            //         // console.log(t.icon_url)
 
-                    console.log(`${value} valeur > ${key}`)
+            //         console.log(`${value} valeur > ${key}`)
 
-                    if(key == 4) {
-                      return(<Card.Cover source={{ uri: `https://steamcommunity-a.akamaihd.net/economy/image/${t.icon_url}`}} />)
+            //         if(key == 4) {
+            //           return(<Card.Cover source={{ uri: `https://steamcommunity-a.akamaihd.net/economy/image/${t.icon_url}`}} />)
 
-                      // console.log(value)
-                      // [value].map((v, k) => {
-                      //   if (v == "color") {
-                      //     console.log(value[v])
+            //           // console.log(value)
+            //           // [value].map((v, k) => {
+            //           //   if (v == "color") {
+            //           //     console.log(value[v])
 
-                      //   }
+            //           //   }
 
-                      // })
-                  // //     // console.log(Object.keys(c.tags)[key])
-                  // //     // value.map((v) => 
-                  // //     // console.log(v)
-                  // //     // )
-                    }
-                  })
-                }
-            </Card.Content>
-            </Card>
-            )
-          )
+            //           // })
+            //       // //     // console.log(Object.keys(c.tags)[key])
+            //       // //     // value.map((v) => 
+            //       // //     // console.log(v)
+            //       // //     // )
+            //         }
+            //       })
+            // </Card.Content>
+            // </Card>
+          })
         }
+        </ScrollView>
       </ScrollView>
     </SafeAreaView>
   );
